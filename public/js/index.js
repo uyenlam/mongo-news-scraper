579e7d3dc1b3d1cc121cf3980bd9051f61a0456c
Uyen Lam <uyenlam17@u.northwestern.edu>
2017-02-26 23:54:38 -0600

    Added add notes modals and relevant jquery functions

--- public/js/index.js
+++ public/js/index.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
     })
     })
   })
+
+  $('#add-note').click(function(){
+    // grab the id of the article 
+    var articleId = $(this).parent('.content-box').attr('id');
+    // open the corresponding modal
+    $('#modal-' + articleId).modal();
+
+    
+  })
+
+  $('#submit-note').click(function(){
+
+  })
 })
